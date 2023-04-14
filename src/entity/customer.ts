5657247d0602c148c040ccef9998f9ee0ff889f1
Gilson Filho <me@gilsondev.in>
2023-04-14 09:50:54 -0300

    feat: Create a value object - Address

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,13 +1,14 @@
+import Address from "./address";
+
 class Customer {
     _id: string;
     _name: string;
-    _address: string;
+    _address!: Address;
     _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = name;
         this.validateAll();
     }
 
@@ -18,7 +19,7 @@ class Customer {
     }
 
     validateActivate() {
-        if (this._address.length === 0) {
+        if (this._address === undefined) {
             throw new Error("Address is mandatory to activate customer")
         }
     }
@@ -43,5 +44,9 @@ class Customer {
 
     deactivate() {
         this._active = false;
+    }
+
+    set Address(address: Address) {
+        this._address = address
     }
 }
